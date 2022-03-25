39462f826cd915259e76488b1767c90379c1354e
Shubhams-2002 <sssalunkhe_b20@ce.vjti.ac.in>
2022-03-25 10:49:09 +0530

    sample code

--- ayush_app/test/widget_test.dart
+++ ayush_app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:ayush_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(GoogleMapsSampleApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
